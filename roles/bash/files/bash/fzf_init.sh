0df6b447a0a0e0d39f6a9d953b4b19cb3d842bd1
erlenmeyer316 <adam.dickison@posteo.net>
2024-12-30 15:38:16 -0500

    update fzf init, add gpgagent init

--- roles/bash/files/bash/fzf_init.sh
+++ roles/bash/files/bash/fzf_init.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 if [ -f "$HOME/.fzf.bash" ]; then
-    source $HOME/.fzf.bash
+  source $HOME/.fzf.bash
 else
-    if [ $(command -v "fzf") ]; then
-        eval "$(fzf --bash)"
+  if [ $(command -v "fzf") ]; then
+    if [ -f /usr/share/bash-completion/completions/fzf ]; then
+      source /usr/share/bash-completion/completions/fzf
     fi
+
+    if [ -f /usr/share/doc/fzf/examples/key-bindings.bash ]; then
+      source /usr/share/doc/fzf/examples/key-bindings.bash
+    fi
+  fi
 fi
-
-
