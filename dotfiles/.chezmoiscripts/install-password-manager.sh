d362b9b8c748b25280a03d3e43b421fe03045005
erlenmeyer316 <adam.dickison@posteo.net>
2025-07-17 15:51:14 -0400

    Updates

--- dotfiles/.chezmoiscripts/install-password-manager.sh
+++ dotfiles/.chezmoiscripts/install-password-manager.sh
@@ -1,9 +1,19 @@
 #! /bin/sh
 
-# exit immediately if keepassxc-cli is already in $PATH
-type keepassxc-cli > /dev/null 2>&1 && echo "keepassxc is already installed" && exit
 
-case "$(uname -s)" in
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+else
+    if [ -x "$(command -v uname)" ]; then
+        ID=$(uname -s)
+	VERSION_ID=$(uname -r)
+    fi
+fi
+
+# exit immediately if keepassxc-cli is already in $PATH
+type keepassxc-cli > /dev/null 2>&1 && echo "keepassxc is already installed" 
+
+case $ID in
     arch)
 	sudo pacman -Sy keepassxc
 	;;
@@ -15,7 +25,7 @@ case "$(uname -s)" in
 	brew install keepassxc
         ;;
     *)
-	echo "$(uname s) is an unsupported OS"
+	echo "$(uname -s) is an unsupported OS"
 	exit 1
         ;;
 esac
