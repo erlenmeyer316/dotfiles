86e1e53d90d1f8d7be44df6aeba2d633e7054f2d
erlenmeyer316 <adam.dickison@posteo.net>
2025-07-19 08:39:31 -0400

    Adding bash profile

---
+++ dotfiles/dot_bashrc.d/aliases.sh
@@ -0,0 +1,18 @@
+# Navigation & Listing
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls -CF'
+alias r='ranger'
+
+# Git
+alias gs='git status'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git pull'
+alias gd='git diff'
+
+# Editors
+alias v='nvim'
+
+# Docker
+alias d='docker'
