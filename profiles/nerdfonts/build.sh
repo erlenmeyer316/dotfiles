f1a7c3c5c1ecc389eb9d87c512276c9a301263d0
Adam M. Dickison <adam.dickison@posteo.net>
2026-03-16 20:39:35 -0400

    add nerdfonts profile

---
+++ profiles/nerdfonts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FONT_BUILD_DIR="/tmp/dotfiles/downloads/fonts"
+FONT_DIR="$HOME/.fonts"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+while IFS= read -r font_name; do
+	if fc-list --format="%{family}"\n | grep -qi "${font_name}"; then
+           echo "Font ${font_name} already installed..."
+	else
+           curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+           mkdir -p  "${FONT_DIR}"
+           echo "unzip the $font_name.zip"
+           unzip "$font_name.zip" -d "$FONT_DIR/$font_name/"
+	   rm "$font_name.zip" 
+           fc-cache -fv
+           echo "Installed font $font_name"     
+	fi
+done < "${SCRIPT_DIR}/nerd.fontlist"
