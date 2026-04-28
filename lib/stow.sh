904cee5899ac70748909b978767c2939a251a788
Adam M. Dickison <adam.dickison@posteo.net>
2026-04-28 00:24:48 -0400

    pass input vars to stow layer

--- lib/stow.sh
+++ lib/stow.sh
@@ -10,12 +10,12 @@ stow_pkg_exists() { dir_exists "${STOW_DIR}/${1}"; }
 # Run stow on a single package directory.
 # $1 = package name, $2 = stow action flag (-R relink | -D unlink)
 run_stow() {
-    local pkg="$1" action="${2:--R}"
-    if [[ "$DRY_RUN" -eq 1 ]]; then
+    local pkg="$1" action="${2:--R}" dry_run="$3" force="$4"
+    if [[ "$dry_run" -eq 1 ]]; then
         print_always "[dry-run] stow ${action} ${pkg}"
         return
     fi
-    if [[ "$FORCE" -eq 1 ]]; then
+    if [[ "$force" -eq 1 ]]; then
         stow --adopt -d "${STOW_DIR}" -t ~ "${action}" "$pkg"
     else
         stow -d "${STOW_DIR}" -t ~ "${action}" "$pkg"
@@ -24,21 +24,21 @@ run_stow() {
 
 # Stow every package listed in a pkglist file
 link_pkglist() {
-    local pkglist="$1"
+    local pkglist="$1" dry_run="$2" force="$3"
     file_exists "$pkglist" || return 0
     while IFS= read -r pkg; do
         print_msg "  Linking ${pkg}"
-        run_stow "$pkg" -R
+        run_stow "$pkg" -R "${dry_run}" "${force}"
     done < "$pkglist"
 }
 
 # Unstow every package listed in a pkglist file
 unlink_pkglist() {
-    local pkglist="$1"
+    local pkglist="$1" dry_run="$2" force="$3"
     file_exists "$pkglist" || return 0
     while IFS= read -r pkg; do
         print_msg "  Unlinking ${pkg}"
-        run_stow "$pkg" -D
+        run_stow "$pkg" -D "${dry_run}" "${force}"
     done < "$pkglist"
 }
 
