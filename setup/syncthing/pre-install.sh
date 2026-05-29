cb91f7638c2984a27214af2e9d5ebf6a9803cd90
Adam M. Dickison <adam.dickison@posteo.net>
2026-05-29 16:39:03 -0400

    restructure syncthing

---
+++ setup/syncthing/pre-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if command -v syncthing &> /dev/null; then
+    echo "[syncthing] is already installed. Skipping."
+else
+    echo "[syncthing] installing from official apt repo..."
+    if [[ ! -f /etc/apt/sources.list.d/syncthing.list ]]; then
+        sudo mkdir -p /etc/apt/keyrings
+        sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg \
+            https://syncthing.net/release-key.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] \
+https://apt.syncthing.net/ syncthing stable" \
+            | sudo tee /etc/apt/sources.list.d/syncthing.list
+    fi
+fi
